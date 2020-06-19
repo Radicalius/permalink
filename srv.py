5309585b1c374c171009e5f5d2a88c78ce57ac44
Radicalius <mr.zacharycotton@gmail.com>
2020-06-18 23:40:41 -0700

    Get Page Endpoint

--- srv.py
+++ srv.py
@@ -7,12 +7,17 @@ app = Flask(__name__)
 def add():
     url = request.form["url"]
     hash = hashlib.sha256(url.encode("utf8")).hexdigest()
-    print(requests.get(url).headers)
     text = requests.get(url).text
     file = open("pages/{0}".format(hash), "wb")
     file.write(text.encode("utf8"))
     file.close()
     return "http://localhost:8000/{0}".format(hash) , 200
+
+@app.route("/<hash>")
+def get(hash):
+    resp = send_file("pages/{0}".format(hash))
+    resp.headers["Content-Type"] = "text/html"
+    return resp
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000)
