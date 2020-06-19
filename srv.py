4dce18180571efc68449067553554eae417698bf
Radicalius <mr.zacharycotton@gmail.com>
2020-06-18 23:34:01 -0700

    Add Endpoint

--- srv.py
+++ srv.py
@@ -1,7 +1,18 @@
-import flask
-from flask import Flask
+import flask, requests, hashlib
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
+
+@app.route("/add", methods=["POST"])
+def add():
+    url = request.form["url"]
+    hash = hashlib.sha256(url.encode("utf8")).hexdigest()
+    print(requests.get(url).headers)
+    text = requests.get(url).text
+    file = open("pages/{0}".format(hash), "wb")
+    file.write(text.encode("utf8"))
+    file.close()
+    return "http://localhost:8000/{0}".format(hash) , 200
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000)
