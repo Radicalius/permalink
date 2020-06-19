1e8a7334508b767543b0c01da03188a5b7744760
Radicalius <mr.zacharycotton@gmail.com>
2020-06-19 11:47:28 -0700

    Copying to Clipboard

--- extension/background/bg.js
+++ extension/background/bg.js
@@ -1,5 +1,17 @@
+function copyToClipboad(text) {
+  const el = document.createElement('textarea');
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
+
 browser.browserAction.onClicked.addListener((tab) => {
   var xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function() {
+    copyToClipboad(xhttp.responseText)
+  }
   xhttp.open("POST", "http://localhost:8000/add", true)
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   var url = encodeURIComponent(tab.url)
